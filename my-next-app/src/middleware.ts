ed6e75e8abf7bf73d9349ee0cfdf8b71508b5a86
madinabolat <madinabolat01@gmail.com>
2025-02-20 17:46:13 -0500

    config: update middleware, add webhook

--- my-next-app/src/middleware.ts
+++ my-next-app/src/middleware.ts
@@ -2,7 +2,8 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(
   [
-    '/auth(.*)', 
+    '/auth(.*)',
+    '/api/auth(.*)',
     '/'
   ]
 )
