1a35869a6cbb608fe6c3a6d1e0530c64832c97ce
madinabolat <madinabolat01@gmail.com>
2025-02-20 22:17:19 -0500

    config: update sign in sign up pages to redirect to dashboard

---
+++ my-next-app/src/app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+    return (
+    <div>
+      <h1>Hello!</h1>
+      <p>Welcome to your dashboard</p>
+    </div>
+    );
+  }
+  
--- my-next-app/src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ my-next-app/src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,22 @@
+"use client"
+
+import { useEffect } from 'react'
+import { useAuth } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
 import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
+  const { isSignedIn } = useAuth()
+  const router = useRouter()
+  
+  console.log("SignInPage component rendered"); 
+  useEffect(() => {
+    console.log("Checking authentication status")
+    if (isSignedIn) {
+      console.log("User signed in")
+      router.push('/dashboard')
+    }
+  }, [isSignedIn])
+
   return <SignIn />
 }
