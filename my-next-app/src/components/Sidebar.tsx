a7716761c149839f1b26765a9cdd7a2262ba180c
madinabolat <madinabolat01@gmail.com>
2025-02-26 22:28:05 -0500

    Update Sidebar.tsx

--- my-next-app/src/components/Sidebar.tsx
+++ my-next-app/src/components/Sidebar.tsx
@@ -1,27 +1,36 @@
+"use client" 
 import Link from "next/link"
+import { useState } from "react"
 
 export default function Sidebar() {
+    const [isCollapsed, setIsCollapsed] = useState(false)
+
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed)
+
+    }
     return (
-    <div>
-        {/* <h1>Sidebar</h1> */}
+    <div className={`${isCollapsed ? "w-16": "w-64"}`}>
+        {/* <p>toggle is {isCollapsed ? "Collapsed":"Expanded"}</p> */}
+        <button onClick={toggleCollapse}>+</button> 
         <ul>
             <li>
-                <Link href="/dashboard">Dashboard</Link>
+                <Link href="/dashboard">{isCollapsed ? "D": "Dashboard"}</Link>
             </li>
             <li>
-                <Link href="/conversations">Conversations</Link>
+                <Link href="/conversations">{isCollapsed ? "D": "Conversations"}</Link>
             </li>
             <li>
-                <Link href="/integrations">Integrations</Link>
+                <Link href="/integrations">{isCollapsed ? "I": "Integrations"}</Link>
             </li>
             <li>
-                <Link href="/appointments">Appointments</Link>
+                <Link href="/appointments">{isCollapsed ? "A": "Appointments"}</Link>
             </li>
             <li>
-                <Link href="/emailmarketing">Email Marketing</Link>
+                <Link href="/emailmarketing">{isCollapsed ? "E": "Email Marketing"}</Link>
             </li>
             <li>
-                <Link href="/settings">Settings</Link>
+                <Link href="/settings">{isCollapsed ? "S": "Settings"}</Link>
             </li>
         </ul>
     </div>
