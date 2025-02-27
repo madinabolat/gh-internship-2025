6e9e757f90616d913e1c2bab49dbf524690645a2
madinabolat <madinabolat01@gmail.com>
2025-02-27 13:54:08 -0500

    icons

--- my-next-app/src/components/Sidebar.tsx
+++ my-next-app/src/components/Sidebar.tsx
@@ -1,6 +1,9 @@
 "use client" 
 import Link from "next/link"
 import { useState } from "react"
+import { RiMenuLine, RiCloseLine } from "react-icons/ri"
+import { HiOutlineViewGrid, HiChatAlt2, HiAdjustments, HiCalendar, HiMail, HiCog } from "react-icons/hi";
+
 
 export default function Sidebar() {
     const [isCollapsed, setIsCollapsed] = useState(false)
@@ -12,25 +15,33 @@ export default function Sidebar() {
     return (
     <div className={`${isCollapsed ? "w-16": "w-64"}`}>
         {/* <p>toggle is {isCollapsed ? "Collapsed":"Expanded"}</p> */}
-        <button onClick={toggleCollapse}>+</button> 
+        <button 
+        onClick={toggleCollapse}>
+        {isCollapsed? <RiCloseLine size = {24}/> :  
+        <div className="flex items-center gap-2">
+        <span>MailGenie</span>
+        {isCollapsed ? <RiCloseLine size={24} /> : <RiMenuLine size={24} />}
+      </div>
+        }
+        </button> 
         <ul>
             <li>
-                <Link href="/dashboard">{isCollapsed ? "D": "Dashboard"}</Link>
+                <Link href="/dashboard">{isCollapsed ? <HiOutlineViewGrid size = {24}/>: "Dashboard"}</Link>
             </li>
             <li>
-                <Link href="/conversations">{isCollapsed ? "D": "Conversations"}</Link>
+                <Link href="/conversations">{isCollapsed ? <HiChatAlt2 size = {24}/>: "Conversations"}</Link>
             </li>
             <li>
-                <Link href="/integrations">{isCollapsed ? "I": "Integrations"}</Link>
+                <Link href="/integrations">{isCollapsed ? <HiAdjustments size = {24}/>: "Integrations"}</Link>
             </li>
             <li>
-                <Link href="/appointments">{isCollapsed ? "A": "Appointments"}</Link>
+                <Link href="/appointments">{isCollapsed ? <HiCalendar size = {24}/>: "Appointments"}</Link>
             </li>
             <li>
-                <Link href="/emailmarketing">{isCollapsed ? "E": "Email Marketing"}</Link>
+                <Link href="/emailmarketing">{isCollapsed ? <HiMail size = {24}/>: "Email Marketing"}</Link>
             </li>
             <li>
-                <Link href="/settings">{isCollapsed ? "S": "Settings"}</Link>
+                <Link href="/settings">{isCollapsed ? <HiCog size = {24}/>: "Settings"}</Link>
             </li>
         </ul>
     </div>
